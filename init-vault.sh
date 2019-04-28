8b4b8c50fc04c16cfb88e1a3e4bc4827db880ec0
Valdas Mazrimas <valdas.mazrimas@gmail.com>
2019-04-28 22:36:06 +0300

    Update scripts for OC 3.11 and Vault 1.1.1

--- init-vault.sh
+++ init-vault.sh
@@ -2,24 +2,26 @@
 set -e
 set -o pipefail
 
+namespace=${1-"secrets-management"}
+
 function vault_enable_postgres_backend {
   echo "Enabling postgres backend..."
   vault secrets enable database
   # Create a database configuration with a superuser on database 'backend'
   vault write database/config/postgres-db plugin_name=postgresql-database-plugin allowed_roles="*" connection_url="postgresql://postgres:somegeneratedpassword@postgresql:5432/backend?sslmode=disable"
   # Create a role which creates a PostgreSQL role with all permissions on database 'backend'
-  vault write database/roles/backend db_name=postgres-db    creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO \"{{name}}\";
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\""  default_ttl="5m"    max_ttl="9000h"
+  vault write database/roles/backend db_name=postgres-db creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO \"{{name}}\";
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\"" default_ttl="5m" max_ttl="9000h"
   echo "Writing sample secret..."
   # Write a sample generic secret for later retrieval
-  vault write secret/backend password=pwd  
+  vault write secret/backend password=pwd
 }
 
-function vault_enable_kubernets_auth {
+function vault_enable_kubernetes_auth {
   echo "Creating service account for Vault..."
   oc create sa vault-auth
   # Add policy to allow TokenReview requests from vault
-  oc adm policy add-cluster-role-to-user system:auth-delegator -z vault-auth
+  oc adm policy add-cluster-role-to-user system:auth-delegator system:serviceaccount:$namespace:vault-auth
   # Get the JWT token for the servie account
   reviewer_service_account_jwt=$(oc serviceaccounts get-token vault-auth)
   # Get the CA certificate for our cluster
@@ -31,11 +33,13 @@ function vault_enable_kubernets_auth {
   vault write auth/kubernetes/config token_reviewer_jwt=$reviewer_service_account_jwt kubernetes_host=https://kubernetes.default.svc.cluster.local:443 kubernetes_ca_cert=@ca.crt 
   rm ca.crt
   echo "Creating sample policy with name 'backend'"
-  vault policy-write backend vault/backend-policy.hcl 
-  echo "Creating sample role with name 'backend' bound to namespace 'secret-management'"
-  vault write auth/kubernetes/role/backend bound_service_account_names=default bound_service_account_namespaces=secret-management policies=backend ttl=2h
+  vault policy write backend vault/backend-policy.hcl
+  echo "Creating sample role with name 'backend' bound to namespace ${namespace}"
+  vault write auth/kubernetes/role/backend bound_service_account_names=default bound_service_account_namespaces=$namespace policies=backend ttl=2h
+  echo "Enabling KV for secret path"
+  vault secrets enable -path=secret kv
 }
 
 export VAULT_SKIP_VERIFY=true
-vault_enable_kubernets_auth
+vault_enable_kubernetes_auth
 vault_enable_postgres_backend
